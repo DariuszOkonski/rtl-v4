5c0bfec2a7e6faf45a5e2682b9597d383e211c1b
Dariusz Okoński <darek200180@gmail.com>
2024-09-11 21:10:21 +0200

    initialization

---
+++ src/UserForm.test.js
@@ -0,0 +1,53 @@
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import UserForm from './UserForm';
+
+test('it shows two inputs and a button', () => {
+  render(<UserForm />);
+
+  const inputs = screen.getAllByRole('textbox');
+  const button = screen.getByRole('button');
+
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
+
+test('it calls onUserAdd when the form is submitted v1', async () => {
+  // not the best implementation
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+  render(<UserForm onUserAdd={callback} />);
+
+  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+  await user.click(nameInput);
+  await user.keyboard('jane');
+  await user.click(emailInput);
+  await user.keyboard('jane@jane.com');
+
+  const button = screen.getByRole('button');
+  await user.click(button);
+
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({ name: 'jane', email: 'jane@jane.com' });
+});
+
+test('it calls onUserAdd when the form is submitted v2', async () => {
+  const mock = jest.fn();
+  render(<UserForm onUserAdd={mock} />);
+
+  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+  await user.click(nameInput);
+  await user.keyboard('jane');
+  await user.click(emailInput);
+  await user.keyboard('jane@jane.com');
+
+  const button = screen.getByRole('button');
+  await user.click(button);
+
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({ name: 'jane', email: 'jane@jane.com' });
+});
