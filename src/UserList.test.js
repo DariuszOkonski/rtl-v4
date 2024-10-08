215e6debeb9dda9e368bc083d2c564eaf1bca5e9
Dariusz Okoński <darek200180@gmail.com>
2024-10-08 19:28:22 +0200

    add test

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -6,16 +6,25 @@ const users = [
   { name: 'sam', email: 'sam@sam.com' },
 ];
 
-test('render one row per user', () => {
+test('render one row per user v1', () => {
   render(<UserList users={users} />);
   const rows = screen.getAllByTestId('user');
   expect(rows).toHaveLength(2);
 });
 
-test('render on row per user v2', () => {
+test('render one row per user v2', () => {
   render(<UserList users={users} />);
   const rows = within(screen.getByTestId('users')).getAllByRole('row');
   expect(rows).toHaveLength(2);
 });
 
-test.skip('render the email and name of each user', () => {});
+test('render one row per user v3', () => {
+  const { container } = render(<UserList users={users} />);
+  // eslint-disable-next-line
+  const rows = container.querySelectorAll('tbody tr');
+  expect(rows).toHaveLength(2);
+});
+
+test('render the email and name of each user v1', () => {
+  render(<UserList users={users} />);
+});
